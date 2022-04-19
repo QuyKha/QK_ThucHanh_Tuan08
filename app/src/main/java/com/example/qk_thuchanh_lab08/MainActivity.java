5e8d8b2520714d0a45ad325b93f2261317d025ca
QKDev <nguyenquykha123@gmail.com>
2022-04-19 22:58:50 +0700

    Fixed

---
+++ app/src/main/java/com/example/qk_thuchanh_lab08/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qk_thuchanh_lab08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
